4cd10d616e0b9a867ab0cbc0e2e69750dbfeb67b
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-30 09:21:50 -0300

    ajustando o swagger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+const express = require('express');
 const envFile = process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev';
 require("dotenv").config({ path: envFile });
 
-const swaggerUi = require("swagger-ui")
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerUi));
-const cors = require('cors');
-const express = require('express');
-const rotas = require('./routers/rotas');
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocs = require('./swagger.json');
 const app = express();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const cors = require('cors');
+const rotas = require('./routers/rotas');
 
 app.use(express.json());
 app.use(rotas);
