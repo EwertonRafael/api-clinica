281c50abb69403a0850f3256cfc434852227358b
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-30 08:58:54 -0300

    ssl dentro do conection

--- src/conection/knex.js
+++ src/conection/knex.js
@@ -4,11 +4,11 @@ const knex = require("knex")({
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
-    database: process.env.DBDATABASE
-  }, ssl: {
-    rejectUnauthorized: false
-  },
-  sslmode: 'require'
+    database: process.env.DBDATABASE,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = knex;
