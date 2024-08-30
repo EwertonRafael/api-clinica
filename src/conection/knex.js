690593a8dbb854e79fd2f446f767437957db6b8e
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-30 08:50:16 -0300

    sslmode

--- src/conection/knex.js
+++ src/conection/knex.js
@@ -1,13 +1,14 @@
 const knex = require("knex")({
   client: "pg",
-  connection:{
+  connection: {
     host: process.env.DBHOST,
     user: process.env.DBUSER,
     password: process.env.DBPASS,
     database: process.env.DBDATABASE
-  },ssl: {
+  }, ssl: {
     rejectUnauthorized: false
-  }
+  },
+  sslmode: 'require'
 });
 
 module.exports = knex;
