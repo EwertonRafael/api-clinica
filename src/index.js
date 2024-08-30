64a84093621d765736638c8e1f8391b64592b0eb
Ewerton Rafael <ton_rafael@hotmail.com>
2024-08-30 09:27:27 -0300

    ajustando o cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const envFile = process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev';
 require("dotenv").config({ path: envFile });
+const cors = require('cors');
 
 const swaggerUi = require("swagger-ui-express")
 const swaggerDocs = require('./swagger.json');
 const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-const cors = require('cors');
 const rotas = require('./routers/rotas');
 
 app.use(express.json());
